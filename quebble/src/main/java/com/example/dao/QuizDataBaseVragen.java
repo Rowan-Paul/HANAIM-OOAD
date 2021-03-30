763537334013aaab75c72a67edcd22a7786dd88b
Rowan-Paul <rpflynn80@gmail.com>
2021-03-30 09:31:06 +0200

    Add more meerkeuzevragen & quizzes

--- quebble/src/main/java/com/example/dao/QuizDataBaseVragen.java
+++ quebble/src/main/java/com/example/dao/QuizDataBaseVragen.java
@@ -18,11 +18,54 @@ public class QuizDataBaseVragen {
     Vraag v8 = new VraagKortAntwoord("Wie speelde Mr Freeze in Batman and Robin?",
             "Arnold Schwarzenegger");
 
-    Vraag v9 = new VraagMeerkeuze("Wat is de hoofdstad van Peru?","C",
+    Vraag v9 = new VraagMeerkeuze("Wat is de hoofdstad van Peru?",
+            "C",
             "A) Arequipa",
             "B) Caracas",
             "C) Lima",
             "D) Santiago");
+    Vraag v10 = new VraagMeerkeuze("Hoe heet de officieuze landenkampioenschap tennis voor mannen?",
+            "A",
+            "A) Davis Cup",
+            "B) Diamond League",
+            "C) Fed Cup",
+            "D) Nations League");
+    Vraag v11 = new VraagMeerkeuze("Wat is de voornaam van de schepper van Miss Marple en Hercule Poirot?",
+            "D",
+            "A) Ruth",
+            "B) Emmy",
+            "C) Joanne",
+            "D) Agatha");
+    Vraag v12 = new VraagMeerkeuze("Welke van de volgende Engelse voetbalclub is afkomstig uit Liverpool?",
+            "C",
+            "A) Arsenal",
+            "B) Chelsea",
+            "C) Everton",
+            "D) Dottenham Hotspur");
+    Vraag v13 = new VraagMeerkeuze("Wat is de hoofdstad van het Franse departement Vaucluse die we kennen uit"+
+            "een heel bekend liedje over een brug?", "B",
+            "A) Parijs",
+            "B) Avignon",
+            "C) Keulen",
+            "D) Port-au-Prince");
+    Vraag v14 = new VraagMeerkeuze("Welke band stond in 2008 in de hitparade met de song Viva La Vida?",
+            "C",
+            "A) Kensington",
+            "B) One Direction",
+            "C) Coldplay",
+            "D) Hotplay");
+    Vraag v15 = new VraagMeerkeuze("Wat is de bijnaam van de Conservatieve Partij in Engeland?",
+            "A",
+            "A) Tory, Torries, The Tories",
+            "B) Red hats",
+            "C) Tobies",
+            "D) Tonies");
+    Vraag v16 = new VraagMeerkeuze("Wat stad ligt in twee continenten?",
+            "A",
+            "A) Instanboel",
+            "B) Istanbon",
+            "C) Constantinopel",
+            "D) shenzhen");
 
     public ArrayList<Vraag> getAllVragen() {
         ArrayList<Vraag> vragen = new ArrayList<>();
@@ -37,6 +80,13 @@ public class QuizDataBaseVragen {
         vragen.add(v7);
         vragen.add(v8);
         vragen.add(v9);
+        vragen.add(v10);
+        vragen.add(v11);
+        vragen.add(v12);
+        vragen.add(v13);
+        vragen.add(v14);
+        vragen.add(v15);
+        vragen.add(v16);
 
         return vragen;
     }
--- quebble/src/main/java/com/example/dao/QuizDatabaseMock.java
+++ quebble/src/main/java/com/example/dao/QuizDatabaseMock.java
@@ -8,29 +8,38 @@ import java.util.List;
 import java.util.Random;
 
 public class QuizDatabaseMock implements QuizDatabase {
-    private ArrayList<Quiz> quizzes = new ArrayList();
     QuizDataBaseVragen vragenDB = new QuizDataBaseVragen();
     Random r = new Random();
+    private ArrayList<Quiz> quizzes = new ArrayList();
 
     public ArrayList getQuizzes() {
+        Quiz q1 = new Quiz(newQuiz());
+        Quiz q2 = new Quiz(newQuiz());
+        Quiz q3 = new Quiz(newQuiz());
+        Quiz q4 = new Quiz(newQuiz());
+        quizzes.add(q1);
+        quizzes.add(q2);
+        quizzes.add(q3);
+        quizzes.add(q4);
+
+        return quizzes;
+    }
+
+    public ArrayList<Vraag> newQuiz() {
         List<Vraag> vragen = vragenDB.getAllVragen();
         ArrayList<Vraag> quizVragen = new ArrayList<>();
 
-        for(int i = 0; i < 8; ++i) {
+        for (int i = 0; i < 8; ++i) {
             int randomint = r.nextInt(vragen.size());
 
             // check if vraag exists in quizVragen
-            if(quizVragen.contains(vragen.get(randomint))) {
+            if (quizVragen.contains(vragen.get(randomint))) {
                 --i;
             } else {
-                System.out.println(randomint);
                 quizVragen.add(vragen.get(randomint));
             }
         }
 
-        Quiz q1 = new Quiz(quizVragen);
-        quizzes.add(q1);
-
-        return quizzes;
+        return quizVragen;
     }
 }
